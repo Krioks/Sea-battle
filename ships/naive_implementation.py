56b4781c34103ef8adf25f6ca9311cec388d2de4
Krioks <core.safecrlf>
2021-11-21 14:17:59 +0300

    refactor naive_implementation, add ArgumentParser

--- ships/naive_implementation.py
+++ ships/naive_implementation.py
@@ -1,13 +1,19 @@
+import argparse
+
+
 # naive implementation
+from pathlib import Path
+
 
 def get_list_of_ships() -> list:
-    with open('examples_of_maps/new_map.txt', 'r') as f:
+    path_to_file = Path(args.path)
+    full_path_to_file = Path(__file__).parent.parent / path_to_file
+    with open(full_path_to_file, 'r') as f:
         fields = f.read()
     return list(map(list, fields.splitlines()))
 
 
-def file_check() -> list:
-    list_of_ships = get_list_of_ships()
+def list_check(list_of_ships: list) -> None:
     map_size = len(list_of_ships)
     for i in range(1, map_size):
         for j in range(1, map_size):
@@ -18,15 +24,25 @@ def file_check() -> list:
                         j - 1] == '#') and (
                             list_of_ships[i][j] == '#' or list_of_ships[i - 1][j - 1] == '#')):
                 raise RuntimeError
-    return list_of_ships
 
 
 def ships_counter(list_of_ships, final_array):
+    list_check(list_of_ships)
     map_size = len(list_of_ships)
     single_doubles = False
     if final_array:
         single_doubles = True
     ship_size = 0
+
+    if map_size == 1:
+        if list_of_ships[0][0] == '#':
+            if not single_doubles:
+                final_array.append(1)
+                return final_array
+            else:
+                return final_array
+        else:
+            return final_array
 
     def append_non_single_ship(ship_size_):
         if ship_size_:
@@ -104,7 +120,14 @@ def ships_counter(list_of_ships, final_array):
 
 
 if __name__ == '__main__':
-    primary_list_of_ships = file_check()
+    parser = argparse.ArgumentParser(description='Process some path.')
+    parser.add_argument('-path',
+                        type=str,
+                        help='Path to example file',
+                        default='examples_of_maps/5.txt')
+    args = parser.parse_args()
+
+    primary_list_of_ships = get_list_of_ships()
     transpose_list_of_ships = list(zip(*primary_list_of_ships))
     length_of_all_ships = []
 
